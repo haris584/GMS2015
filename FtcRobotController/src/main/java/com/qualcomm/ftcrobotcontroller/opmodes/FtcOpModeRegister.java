84a3636979d13009c921483de3e774fc998aa175
haris584 <eridanampora33@gmail.com>
2015-10-30 17:06:00 -0400

    Update FtcOpModeRegister.java
    registers driveOp mode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -59,8 +59,9 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    manager.register("PushBotAuto", PushBotAuto.class);
+    manager.register("PushBotManual", PushBotManual.class);
+    manager.register("DriveOp", driveOp.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
